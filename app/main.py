66a2734fcacaf2a90b562804e3dd14a9f4421bf6
Hamza-P <hamza.siddiqui4747@gmail.com>
2024-09-26 00:00:26 +0500

    file path in progress

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from modules.regex import email_extract
+import modules.extractors as extractor
+
 
 app = Flask(__name__)
 
@@ -7,7 +9,15 @@ app = Flask(__name__)
 def root_route():
     return render_template('index.html')
 
-# print(email_extract('dfdfnd hamza@gmial.ds'))
+@app.route('/api/extract', methods=['POST'])
+def extract_route():
+    data = request.get_json()
+    print("My Data", data)
+
+    response = {"message":"success!"}
+    return jsonify(response), 200
+# print(len( extract_text_from_url('https://hamzasiddiqui.netlify.app/')))
+# print(email_extract(extract_text_from_url('https://hamzasiddiqui.netlify.app/')))
 
 if __name__ == '__main__':
     app.run(debug=True)
